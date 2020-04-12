928534a74465661f05dcbe2030609d7d0718eb65
悠 <54658896+yuu-no@users.noreply.github.com>
2020-04-12 20:11:22 +0900

    Update sample.js

--- sample.js
+++ sample.js
@@ -3,7 +3,7 @@ alert("test");
 //CSVファイルを読み込む関数getCSV()の定義
 function getCSV(){
     var req = new XMLHttpRequest(); // HTTPでファイルを読み込むためのXMLHttpRrequestオブジェクトを生成
-    req.open("get", "adjective.csv", true); // アクセスするファイルを指定
+    req.open("get", "adjective.csv"); // アクセスするファイルを指定
     req.send(null); // HTTPリクエストの発行
 	
     // レスポンスが返ってきたらconvertCSVtoArray()を呼ぶ	
@@ -15,7 +15,6 @@ function getCSV(){
 // 読み込んだCSVデータを二次元配列に変換する関数convertCSVtoArray()の定義
 function convertCSVtoArray(str){ // 読み込んだCSVデータが文字列として渡される
     var tmp = str.split("\n"); // 改行を区切り文字として行を要素とした配列を生成
- 
     alert(tmp[1]);
 }
  
