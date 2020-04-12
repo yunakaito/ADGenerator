4edef0f46aa7d781b70da7728a4c0639922e5606
悠 <54658896+yuu-no@users.noreply.github.com>
2020-04-12 20:03:56 +0900

    Update sample.js

--- sample.js
+++ sample.js
@@ -17,12 +17,7 @@ function convertCSVtoArray(str){ // 読み込んだCSVデータが文字列と�
     var result = []; // 最終的な二次元配列を入れるための配列
     var tmp = str.split("\n"); // 改行を区切り文字として行を要素とした配列を生成
  
-    // 各行ごとにカンマで区切った文字列を要素とした二次元配列を生成
-    for(var i=0;i<tmp.length;++i){
-        result[i] = tmp[i].split(',');
-    }
- 
-    alert(result[1][2]); // 300yen
+    alert(result[1]);
 }
  
 getCSV(); //最初に実行される
