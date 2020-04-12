3e7d3fc8a0f1f8d8a7af8984f5a005ce6df0d6b5
悠 <54658896+yuu-no@users.noreply.github.com>
2020-04-12 20:37:37 +0900

    Update function.js

--- function.js
+++ function.js
@@ -12,7 +12,7 @@ function getCSV() {
 
 // 読み込んだCSVデータを二次元配列に変換する関数convertCSVtoArray()の定義
 function convertCSVtoArray(str) { // 読み込んだCSVデータが文字列として渡される
-    var tmp = str.split("\n"); // 改行を区切り文字として行を要素とした配列を生成z
+    var tmp = str.split("\n"); // 改行を区切り文字として行を要素とした配列を生成
     var adj = document.getElementById('adj');
     adj.innerHTML = '<p>' + tmp[getRandomInt(0, tmp.length)] + '</p>';
 }
@@ -24,3 +24,8 @@ function getRandomInt(min, max) {
 }
 
 getCSV(); //最初に実行される
+
+
+document.onclick = function() {
+  getCSV();
+};
