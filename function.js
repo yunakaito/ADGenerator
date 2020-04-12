837bbfb7e86583947078b19f7fdc2bbcdc27e1d3
悠 <54658896+yuu-no@users.noreply.github.com>
2020-04-12 20:33:23 +0900

    Update function.js

--- function.js
+++ function.js
@@ -1,20 +1,26 @@
 //CSVファイルを読み込む関数getCSV()の定義
-function getCSV(){
+function getCSV() {
     var req = new XMLHttpRequest(); // HTTPでファイルを読み込むためのXMLHttpRrequestオブジェクトを生成
     req.open("get", "adjective.csv"); // アクセスするファイルを指定
     req.send(null); // HTTPリクエストの発行
-	
+
     // レスポンスが返ってきたらconvertCSVtoArray()を呼ぶ	
-    req.onload = function(){
-	convertCSVtoArray(req.responseText); // 渡されるのは読み込んだCSVデータ
+    req.onload = function () {
+        convertCSVtoArray(req.responseText); // 渡されるのは読み込んだCSVデータ
     }
 }
- 
+
 // 読み込んだCSVデータを二次元配列に変換する関数convertCSVtoArray()の定義
-function convertCSVtoArray(str){ // 読み込んだCSVデータが文字列として渡される
+function convertCSVtoArray(str) { // 読み込んだCSVデータが文字列として渡される
     var tmp = str.split("\n"); // 改行を区切り文字として行を要素とした配列を生成z
-	var adj = document.getElementById('adj');
-        adj.innerHTML = '<p>'+tmp[1]+'</p>';
+    var adj = document.getElementById('adj');
+    adj.innerHTML = '<p>' + tmp[getRandomInt(0, tmp.length)] + '</p>';
 }
- 
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
+
 getCSV(); //最初に実行される
